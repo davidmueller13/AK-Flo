b75e4b8c31c71f181412d69957c3737d4dd45c88
anarkia1976 <stefano.villa1976@gmail.com>
2014-05-25 13:55:37 +0200

    cpufreq: stockdemand: sync_freq = 702000 + optimal_freq = 1350000

--- drivers/cpufreq/cpufreq_stockdemand.c
+++ drivers/cpufreq/cpufreq_stockdemand.c
@@ -144,8 +144,8 @@ static struct dbs_tuners {
 	.up_threshold_any_cpu_load = DEF_FREQUENCY_UP_THRESHOLD,
 	.ignore_nice = 0,
 	.powersave_bias = 0,
-	.sync_freq = 0,
-	.optimal_freq = 0,
+	.sync_freq = 702000,
+	.optimal_freq = 1350000,
 };
 
 static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
